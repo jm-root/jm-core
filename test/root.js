28337d9a8d234ddd33216626c10155ee1444fe9f
jeff <jeff@jamma.cn>
2017-03-25 12:38:30 +0800

    global test

--- test/root.js
+++ test/root.js
@@ -2,8 +2,15 @@ import chai from 'chai';
 let expect = chai.expect;
 import JM from '../src';
 
-let jm = JM();
 describe('root', function() {
+    let jm = JM();
+    it('jm.root', function() {
+        expect(jm.root).to.be.an('object');
+        expect(jm.root.registries).to.be.an('object');
+    });
+});
+
+describe('global', function() {
     it('jm.root', function() {
         expect(jm.root).to.be.an('object');
         expect(jm.root.registries).to.be.an('object');
