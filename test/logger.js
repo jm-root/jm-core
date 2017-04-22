628650ce0fa2d9f6111a0a7fdc3935006203958f
jeff <jeff@jamma.cn>
2017-04-22 19:34:47 +0800

    优化err

--- test/logger.js
+++ test/logger.js
@@ -1,27 +1,24 @@
 import chai from 'chai';
 let expect = chai.expect;
 import $ from '../src';
-import Logger from '../src/logger';
 
 jm = new $();
 
-let log = function (logger) {
-    ['debug', 'info', 'warn', 'error', 'fatal'].forEach(function (level) {
-        logger[level] && (logger[level](`${level}`));
-    });
-};
-
 describe('logger', function () {
     it('jm.logger', function () {
-        expect(jm.logger).to.be.an('object');
-        expect(Logger).to.be.an('function');
-        log(jm.logger);
+        let logger = jm.logger;
+        expect(logger).to.be.an('object');
+        for(let level of ['debug', 'info', 'warn', 'error']) {
+            expect(logger[level]).to.be.an('function');
+        };
     });
 
     it('jm.getLogger', function () {
         expect(jm.getLogger).to.be.a('function');
         let logger = jm.getLogger('main');
         expect(logger).to.be.an('object');
-        log(logger);
+        for(let level of ['debug', 'info', 'warn', 'error']) {
+            expect(logger[level]).to.be.an('function');
+        };
     });
 });
